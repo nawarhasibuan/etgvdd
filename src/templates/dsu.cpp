ca1a8ec2ff4ef693a825116e23b884f8d9f9b7f0
nawarhasibuan <panawarhsb28@gmail.com>
2025-07-03 20:59:45 +0700

    feat(template): add dsu template

---
+++ src/templates/dsu.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+using namespace std;
+struct DSU {
+    vector<int> parent, rank;
+
+    DSU(int n) {
+        parent.resize(n);
+        rank.resize(n, 0);
+        for (int i = 0; i < n; i++) parent[i] = i;
+    }
+
+    int find(int u) {
+        if (parent[u] != u) parent[u] = find(parent[u]);
+        return parent[u];
+    }
+
+    void unite(int u, int v) {
+        u = find(u);
+        v = find(v);
+        if (u == v) return;
+        if (rank[u] < rank[v]) swap(u, v);
+        parent[v] = u;
+        if (rank[u] == rank[v]) rank[u]++;
+    }
+};
+int main() {
+    return 0;
+}
