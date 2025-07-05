5510695e10991f049c028f0b4a2e7e02b1dc764c
nawarhasibuan <panawarhsb28@gmail.com>
2025-07-06 00:53:00 +0700

    fix(osp2024): correct range index of searching in solution C

--- src/contests/osn/osp2024/C.cpp
+++ src/contests/osn/osp2024/C.cpp
@@ -4,18 +4,23 @@
 using namespace std;
 
 long long tebas(vector<int> A, int h);
-int tebang(vector<int> A, long long M);
+int tebang(vector<int> A, long long M, int max_val);
 int main() {
     int N;
-    long M;
+    long long M;
+    int max_val;
     vector<int> A;
     cin >> N >> M;
-    for (int i = 0; i < N; i++) {
-        int x;
+    int x;
+    cin >> x;
+    A.push_back(x);
+    max_val = x;
+    for (int i = 1; i < N; i++) {
+        max_val = (x > max_val) ? x : max_val;
         cin >> x;
         A.push_back(x);
     }
-    cout << tebang(A, M) << endl;
+    cout << tebang(A, M, max_val) << endl;
 
     return 0;
 }
@@ -27,9 +32,9 @@ long long tebas(vector<int> A, int h) {
 
     return sum;
 }
-int tebang(vector<int> A, long long M) {
+int tebang(vector<int> A, long long M, int max_val) {
     int l = 0;
-    int r = A.size();
+    int r = max_val;
     while (l < r) {
         int pohon = (l + r) / 2;
         if (tebas(A, pohon) > M) {
