088eb0cdac37824c64805ca625b08db45463b6ce
Panawar H <panawarhsb28@gmail.com>
2026-07-16 10:10:35 +0700

    feat(osn) : osnp 2023 bagian B nomor 3

--- src/contests/osn/osp2023/C.cpp
+++ src/contests/osn/osp2023/C.cpp
@@ -1,33 +1,65 @@
-#include <iostream>
-#include <vector>
-
+#include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+
+vector<vector<long long>> adj;
+vector<long long> a;
+vector<long long> b;
+vector<long long> kesulitan;
+vector<bool> visited;
+
+/**
+ * mencari kesulitan paling kecil di tiap kelompok
+ */
+long long dfs(long long start) {
+    visited[start] = true;
+    long long k = a[start]; // init kesulitan start (dirinya sendiri)
+    for (auto child : adj[start]) {
+        if (!visited[child]) {
+            long long biaya_child = dfs(child);
+            k = min(k, biaya_child);
+        }
+    }
+    return k;
+}
 
 int main() {
     // input
-    int n, m, k;
+    long long n, m, k;
     cin >> n >> m >> k;
-    vector<int> a(n);
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
-    }
-    vector<int> b(m);
-    for (int i = 0; i < m; i++) {
-        cin >> b[i];
-    }
-    vector<vector<int>> e;
-    for (int i = 0; i < k; i++) {
-        int u, v;
+    a.resize(n + 1);
+    b.resize(m);
+    adj.resize(n + 1);
+    for (long long i = 1; i <= n; i++) cin >> a[i];
+    for (long long i = 0; i < m; i++) cin >> b[i];
+    while (k--) {
+        long long u, v;
         cin >> u >> v;
-        e.at(u).push_back(v);
-        e.at(v).push_back(u);
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
 
-    // algoritma
+    visited.resize(n + 1);
+    for (long long i = 1; i <= n; i++) visited[i] = false;
 
-    // output
-
-    cout << 0 << endl;
-
+    for (long long i = 1; i <= n; i++) {
+        if (!visited[i]) {
+            long long x = dfs(i);
+            kesulitan.push_back(x);
+        }
+    }
+    sort(kesulitan.begin(), kesulitan.end(), greater<long long>());
+    sort(b.begin(), b.end());
+    if (kesulitan.size() > b.size())
+        cout << -1 << endl;
+    else {
+        long long biaya = 0;
+        for (long long i = 0; i < kesulitan.size(); i++) {
+            long long p = kesulitan[i];
+            long long q = b[i];
+            biaya += p * q;
+        }
+        cout << endl << biaya << endl;
+    }
     return 0;
 }
